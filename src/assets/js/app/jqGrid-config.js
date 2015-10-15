ccee65d402551f5474b559889e37d0f7902d22ed
Balázs Meskó <meskobalazs@gmail.com>
2015-10-15 15:45:09 +0200

    App.js szétszedése általános és specifikus részekre
    Az app.js fájlt szétszedtem egy general.js és app.js fájlokra. Előbbit
    minden oldalon importálhatunk mert kellhet (a formGeneral az szinte
    minden, a jqGrid az sokszor). Utóbbiban a demóoldalakhoz szükséges
    dolgok maradtak csak.

---
+++ src/assets/js/app/jqGrid-config.js
@@ -0,0 +1,14 @@
+$(function () {
+    $.extend($.jgrid.defaults, {
+        autowidth :true,
+        responsive: true,
+        styleUI: 'Bootstrap'
+    });
+
+
+    $.jgrid.styleUI.Bootstrap.base.headerTable = "table  table-condensed";
+    $.jgrid.styleUI.Bootstrap.base.rowTable = "table  table-condensed table-striped table-hover";
+    $.jgrid.styleUI.Bootstrap.base.footerTable = "table ";
+    $.jgrid.styleUI.Bootstrap.base.pagerTable = "table table-condensed";
+    $.jgrid.styleUI.Bootstrap.common.highlight = "highlight";
+})
